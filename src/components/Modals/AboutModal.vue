df0a88341491f80228a0715c3dee90f8a633be5c
Aid1043 <agames@lachac.org>
2025-12-07 09:41:25 -0600

    Disclaimer; minor updates

--- src/components/Modals/AboutModal.vue
+++ src/components/Modals/AboutModal.vue
@@ -49,6 +49,7 @@ onMounted(()=>{
       <div class="text">Don't forget about those additional arrangements! They might check off categories that the original song wouldn't on its own.</div>
     </div>
   </div>
+  <p>Kingdom Hearts Heed to the Pulse not affiliated with the Walt Disney Company, Square Enix, or thier subsidiaries.</p>
 </template>
 
 <style scoped>
